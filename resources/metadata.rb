a465447c6ea29a3bfc9f26ef0f90a46c1d4c60df
Luis Blanco <ljblanco@redborder.com>
2023-10-24 10:17:07 +0100

    fix curl

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'dvanhoucke@redborder.com'
 license          'All rights reserved'
 description      'Installs/Configures cookbook-dswatcher'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.5'
+version          '0.0.6'
